a1713288123c3457dc1384431b65744081781212
leprecon73 <leprecon73@gmail.com>
2023-04-09 23:42:44 +0400

    3.2.2

--- 3.2/README.md
+++ 3.2/README.md
@@ -1,2 +1,6 @@
 ex 3.2
+You can modify index.js, then "commit" and eventually it will be deployed to the render.com cloud at:
 
+https://three-2-j6lm.onrender.com/
+
+just wait 30-40 seconds after 'git push'
--- 3.2/index.js
+++ 3.2/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Changes!</h1>')
+  res.send('<h1>Hello new Changes with deploy to render.com cloud! </h1>')
 })
 
 const PORT = 8080
